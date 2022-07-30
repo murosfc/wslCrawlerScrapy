1f7d42b6341a0d488ed1db443016039a5f4e4fb5
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2022-07-30 14:41:04 -0300

    Crawler finalizado

--- wsl/spiders/wsl.py
+++ wsl/spiders/wsl.py
@@ -1,27 +1,35 @@
 import scrapy
+from datetime import date
+import pandas as pd
 
 class PapelSpider(scrapy.Spider):
     name = 'wsl'
-    start_url = 'http://www.worldsurfleague.com/athletes/tour/mct?year=2022'
+    start_url = 'http://www.worldsurfleague.com/athletes'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
 
     def start_requests(self):
-        yield scrapy.Request(self.start_url, headers=self.headers)
+        current_tour_url = self.start_url +'/tour/mct?year=' + str(date.today().year)
+        yield scrapy.Request(current_tour_url, headers=self.headers)
 
     def parse(self, response):
+        ranking = {}
         athletes = response.xpath('//*[contains(@class,"tableType-athlete")]//tr')
-        for surfer in athletes:
-            rank = surfer.css("td.athlete-rank::text").get()
-            name = surfer.css("a.athlete-name::text").get()
-            total_points = surfer.css("span.tour-points::text").get()
-            if rank is not None:
-                yield {
-                'rank': rank,
-                'name': name,
-                'total points': total_points,
-                }
-
-
-
-
+        for i, surfer in enumerate(athletes):
+            if i > 0:
+                ranking['rank'] = surfer.css("td.athlete-rank::text").get()
+                print("\nRANK: ", ranking['rank'])
+                ranking['name'] = surfer.css("a.athlete-name::text").get()
+                ranking['country'] = surfer.css("span.athlete-country-name::text").get()
+                ranking['total_points'] = surfer.css("span.tour-points::text").get()
+                rounds = surfer.css("span.tooltip-item::text").getall()
+                if ranking['rank'] is not None:
+                    for j, round in enumerate(rounds):
+                        key = 'round ' + str(j+1)
+                        ranking[key] = round
+                    yield ranking
+        try:
+            df = pd.read_csv("wsl.csv")
+            df.to_excel("wslRank.xlsx")
+        except:
+            pass
 
